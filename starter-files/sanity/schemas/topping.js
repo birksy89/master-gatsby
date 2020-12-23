b14172bf145b56935098f03cd84412780903fcc4
Andrew <andrew@bbm.co.uk>
2020-12-23 14:58:00 +0000

    Match toppings Schema after data import

--- starter-files/sanity/schemas/topping.js
+++ starter-files/sanity/schemas/topping.js
@@ -1,38 +1,36 @@
-import {FaPepperHot as icon} from 'react-icons/fa';
+import { FaPepperHot as icon } from 'react-icons/fa';
 
 export default {
-    // Computer Name
-    name:"topping",
-    // Visible title,
-    title: "Toppings",
-    type: "document",
-    icon : icon,
-    fields: [
-        {
-            name:"name",
-            title: "Topping Name",
-            type:"string",
-            description: "Name of Topping"
-        },
-        {
-            name:"vegitarian",
-            title: "Vegitarian?",
-            type:"boolean",
-            description: "Is this suitable for vegitarians?",
-            options:{
-                layout: "checkbox"
-            }
-        },
-       
-    ],
-    preview: {
-        select:{
-            name:"name",
-            vegitarian: "vegitarian"
-        }
-        ,
-        prepare:(fields) => ({
-            title: `${fields.name} ${fields.vegitarian ?"🌿": ""} `
-        })
-    }
-}
+  // Computer Name
+  name: 'topping',
+  // visible title
+  title: 'Toppings',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Pizza Name',
+      type: 'string',
+      description: 'What is the name of the topping?',
+    },
+    {
+      name: 'vegetarian',
+      title: 'Vegetarian',
+      type: 'boolean',
+      description: 'What is the name of the topping?',
+      options: {
+        layout: 'checkbox',
+      },
+    },
+  ],
+  preview: {
+    select: {
+      name: 'name',
+      vegetarian: 'vegetarian',
+    },
+    prepare: ({ name, vegetarian }) => ({
+      title: `${name} ${vegetarian ? '🌿' : ''}`,
+    }),
+  },
+};
