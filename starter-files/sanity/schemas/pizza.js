fe4a3e9977a480d77a903a23e3289a9989fc94ff
Andrew <andrew@bbm.co.uk>
2020-12-23 09:18:59 +0000

    Creating Data Relationships and Title Preview

--- starter-files/sanity/schemas/pizza.js
+++ starter-files/sanity/schemas/pizza.js
@@ -40,5 +40,41 @@ export default {
             validation: Rule => Rule.min(1000).max(50000),
             // TODO - Add custom input component
         },
-    ]
+        {
+            name:"toppings",
+            title: "Toppings",
+            type: "array",
+            of:[{type: "reference" ,to: [{type: "topping"}]}]
+
+        }
+    ],
+    preview: {
+        select:{
+            title:"name",
+            media: "image",
+            topping0: "toppings.0.name",
+            topping1: "toppings.1.name",
+            topping2: "toppings.2.name",
+            topping3: "toppings.3.name",
+        }
+        ,
+        prepare:({title, media, ...toppings }) => {
+            //  Filter undefined toppings out
+            const toppingsFiltered = []
+            for (const [key, value] of Object.entries(toppings)) {
+                if(value){
+
+                    toppingsFiltered.push(value);
+                }
+              }
+
+            //  Return the preview object for the pizza
+            console.log(toppings)
+           return  {
+            title: `${title} `,
+            media: media,
+            subtitle: toppingsFiltered.join(", ")
+        }
+    }
+    }
 }
